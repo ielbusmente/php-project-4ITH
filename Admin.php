f680702d2569b6bbbb555b9eedd338213b82a809
nmsarm <daneah.sarmiento@yahoo.com>
2021-11-11 17:28:25 +0800

    Admin UI

---
+++ Admin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Admin.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="https://kit.fontawesome.com/274d8634a7.js" crossorigin="anonymous"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <title>Sleepy Admin</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-white">
+
+  <img class="logo" src="assets/sleepy_text.png" height="80" alt="logo"> 
+  <img class="logo2" src="assets/moon.png" height="60"> 
+
+  <div class="collapse navbar-collapse" id="navbar1">
+    <ul class="navbar-nav ml-auto "> 
+        <li class="nav-item active">
+            <a class="nav-link text-dark" href="#"> Inbox &nbsp;
+                <i class="fas fa-envelope" style="color:#D8C47F;"></i>
+                <!-- <span class="sr-only">(current)</span> -->
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-dark" href="#"> Profile &nbsp;
+                <i class="fas fa-user" style="color:#D8C47F;"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="btn ml-2 btn-secondary" href="#">Logout</a>
+        </li>
+    </ul>
+
+  </div>
+</nav>
+
+<div class="container">
+    <h1 class="inquiries-title">Inquiries</h1>
+</div>
+<!-- .//container -->
+
+<!-- <br><br><br><br><br><br><br><br><br><br>
+<article class="bg-secondary mb-3">  
+    <div class="card-body text-center">
+        <h4 class="text-white">HTML UI KIT <br> Ready to use Bootstrap 4 components and templates </h4>
+        <p class="h5 text-white"> for Ecommerce, marketplace, booking websites and product landing pages</p>   
+        <br>
+        <p><a class="btn btn-warning" target="_blank" href="http://bootstrap-ecommerce.com/"> Bootstrap-ecommerce.com  
+        <i class="fa fa-window-restore "></i></a></p>
+    </div>
+    <br><br>
+</article> -->
+
+</body>
+</html>
