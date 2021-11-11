4daaf07e0dcc1d1313e07c740448406e5800353e
ielbusmente <ieljbusmente@gmail.com>
2021-11-11 20:26:18 +0800

    try session and cookie

--- Admin.php
+++ Admin.php
@@ -1,8 +1,17 @@
+<?php
+if (isset($_POST['logout'])) {
+    session_start();
+    setcookie("session-token", "", time() - 1);
+    session_destroy();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,181 +23,196 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
     <title>Sleepy Admin</title>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-white">
+    <?php
+    session_start();
+    if (!(isset($_SESSION['username']) && isset($_SESSION['password'])) || !($_COOKIE['session-token'] === "d93jkdg23jkds")) {
+        setcookie("session-token", "", time() - 1);
+        session_destroy();
+        header('Location: Login.php');
+    }
+    ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-white">
 
-  <img class="logo" src="assets/sleepy_text.png" height="80" alt="logo"> 
-  <img class="logo2" src="assets/moon.png" height="60"> 
+        <img class="logo" src="assets/sleepy_text.png" height="80" alt="logo">
+        <img class="logo2" src="assets/moon.png" height="60">
 
-  <div class="collapse navbar-collapse" id="navbar1">
-    <ul class="navbar-nav ml-auto "> 
-        <li class="nav-item active">
-            <a class="nav-link text-dark" href="#"> Inbox &nbsp;
-                <i class="fas fa-envelope" style="color:#D8C47F;"></i>
-                <!-- <span class="sr-only">(current)</span> -->
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-dark" href="#"> Profile &nbsp;
-                <i class="fas fa-user" style="color:#D8C47F;"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="btn ml-2 btn-secondary" href="#">Logout</a>
-        </li>
-    </ul>
+        <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link text-dark" href="#"> Inbox &nbsp;
+                        <i class="fas fa-envelope" style="color:#D8C47F;"></i>
+                        <!-- <span class="sr-only">(current)</span> -->
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="#"> Profile &nbsp;
+                        <i class="fas fa-user" style="color:#D8C47F;"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                        <input class="btn ml-2 btn-secondary" type="submit" name="logout" value='Logout' />
+                    </form>
+                    <!-- <a class="btn ml-2 btn-secondary" href="# ">Logout</a> -->
+                </li>
+            </ul>
 
-  </div>
-</nav>
-
-<div class="container">
-    <h1 class="inquiries-title">Inquiries</h1>
-</div>
-
-<br><br><br><br><br>
-
-<div class="messaging">
-    <div class="inbox_msg">
-        <div class="mesgs">
-            <div class="msg_history">
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                    <p>Test which is a new approach to have all
-                    solutions</p>
-                    <span class="time_date"> 11:01 AM    |    June 9</span></div>
-                </div>
-            </div>
-            <div class="outgoing_msg">
-                <div class="sent_msg">
-                <p>Test which is a new approach to have all
-                    solutions</p>
-                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
-            </div>
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                    <p>Test, which is a new approach to have</p>
-                    <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
-                </div>
-            </div>
-            <div class="outgoing_msg">
-                <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date"> 11:01 AM    |    Today</span> </div>
-            </div>
-            <div class="incoming_msg">
-                <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="received_msg">
-                <div class="received_withd_msg">
-                    <p>We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.</p>
-                    <span class="time_date"> 11:01 AM    |    Today</span></div>
-                </div>
-            </div>
-            </div>
-            <div class="type_msg">
-            <div class="input_msg_write">
-                <input type="text" class="write_msg" placeholder="Type a message" />
-                <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-            </div>
-            </div>
         </div>
-        <div class="inbox_people">
-            <div class="headind_srch">
-            <div class="recent_heading">
-                <h4>Recent</h4>
-            </div>
-            <div class="srch_bar">
-                <div class="stylish-input-group">
-                <input type="text" class="search-bar"  placeholder="Search" >
-                <span class="input-group-addon">
-                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
-                </span> </div>
-            </div>
-            </div>
-            <div class="inbox_chat">
-            <div class="chat_list active_chat">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
+    </nav>
+
+    <!-- <div class="container"> -->
+    <h1 class="inquiries-title">Inquiries</h1>
+    <!-- </div> -->
+
+    <!-- <br><br><br><br><br> -->
+
+    <div class="messaging">
+        <div class="inbox_msg">
+            <div class="mesgs">
+                <div class="msg_history">
+                    <div class="incoming_msg">
+                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                        <div class="received_msg">
+                            <div class="received_withd_msg">
+                                <p>Test which is a new approach to have all
+                                    solutions</p>
+                                <span class="time_date"> 11:01 AM | June 9</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="outgoing_msg">
+                        <div class="sent_msg">
+                            <p>Test which is a new approach to have all
+                                solutions</p>
+                            <span class="time_date"> 11:01 AM | June 9</span>
+                        </div>
+                    </div>
+                    <div class="incoming_msg">
+                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                        <div class="received_msg">
+                            <div class="received_withd_msg">
+                                <p>Test, which is a new approach to have</p>
+                                <span class="time_date"> 11:01 AM | Yesterday</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="outgoing_msg">
+                        <div class="sent_msg">
+                            <p>Apollo University, Delhi, India Test</p>
+                            <span class="time_date"> 11:01 AM | Today</span>
+                        </div>
+                    </div>
+                    <div class="incoming_msg">
+                        <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                        <div class="received_msg">
+                            <div class="received_withd_msg">
+                                <p>We work directly with our designers and suppliers,
+                                    and sell direct to you, which means quality, exclusive
+                                    products, at a price anyone can afford.</p>
+                                <span class="time_date"> 11:01 AM | Today</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
-                </div>
-            </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
-                </div>
+                <div class="type_msg">
+                    <div class="input_msg_write">
+                        <input type="text" class="write_msg" placeholder="Type a message" />
+                        <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                    </div>
                 </div>
             </div>
-            <div class="chat_list">
-                <div class="chat_people">
-                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
-                <div class="chat_ib">
-                    <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
-                    <p>Test, which is a new approach to have all solutions 
-                    astrology under one roof.</p>
+            <div class="inbox_people">
+                <div class="headind_srch">
+                    <div class="recent_heading">
+                        <h4>Recent</h4>
+                    </div>
+                    <div class="srch_bar">
+                        <div class="stylish-input-group">
+                            <input type="text" class="search-bar" placeholder="Search">
+                            <span class="input-group-addon">
+                                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                            </span>
+                        </div>
+                    </div>
                 </div>
+                <div class="inbox_chat">
+                    <div class="chat_list active_chat">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="chat_list">
+                        <div class="chat_people">
+                            <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="chat_ib">
+                                <h5>Sunil Rajput <span class="chat_date">Dec 25</span></h5>
+                                <p>Test, which is a new approach to have all solutions
+                                    astrology under one roof.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
-
-
-
-
--- Login.php
+++ Login.php
@@ -1,5 +1,18 @@
+<?php
+if (isset($_POST['submit'])) {
+  session_start();
+  $_SESSION['username'] = htmlentities($_POST['username']);
+  $_SESSION['password'] = htmlentities($_POST['password']);
+  $token = "d93jkdg23jkds";
+  setcookie("session-token", $token, time() + 24 * 60 * 60);
+  header('Location: Admin.php');
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Sleepy Login</title>
   <meta charset="utf-8">
@@ -9,32 +22,31 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
 <body>
 
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="assets/sleepy.png" id="icon" alt="Icon" />
+  <div class="wrapper fadeInDown">
+    <div id="formContent">
+      <!-- Icon -->
+      <div class="fadeIn first">
+        <img src="assets/sleepy.png" id="icon" alt="Icon" />
+      </div>
+
+      <!-- Login Form -->
+      <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="text" id="username" class="fadeIn second" name="username" placeholder="Username">
+        <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password">
+        <input type="submit" name="submit" class="fadeIn fourth" value="Log In">
+      </form>
+
+      <!-- Forgot Password -->
+      <div id="formFooter">
+        <a class="underlineHover" href="#">Forgot Password?</a>
+      </div>
+
     </div>
-
-    <!-- Login Form -->
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-      <input type="text" id="username" class="fadeIn second" name="username" placeholder="Username">
-      <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password">
-      <input type="submit" name="submit" class="fadeIn fourth" value="Log In">
-    </form>
-
-    <!-- Forgot Password -->
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
   </div>
-</div>
 
 </body>
+
 </html>
-
-
-
