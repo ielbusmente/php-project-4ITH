e02fa17f274b72cc98a12cc9083f6369e0213420
Richmond Francisco <richmondandrei.francisco.iics@ust.edu.ph>
2021-11-18 17:05:06 +0800

    view form changes

--- Exercise6/index.php
+++ Exercise6/index.php
@@ -20,33 +20,30 @@
             <div class="col-md-12">
                 <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link" id="view-tab" data-toggle="tab" href="#view" role="tab" aria-controls="view" aria-selected="true">View</a>
+                        <a class="nav-link active" id="view-tab" data-toggle="tab" href="#view" role="tab" aria-controls="view" aria-selected="true">View</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" id="insert-tab" data-toggle="tab" href="#insert" role="tab" aria-controls="insert" aria-selected="true">Insert</a>
+                        <a class="nav-link " id="insert-tab" data-toggle="tab" href="#insert" role="tab" aria-controls="insert" aria-selected="true">Insert</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show text-align m-4 form-new" id="view" role="tabpanel" aria-labelledby="view-tab">
+                    <div class="tab-pane fade show text-align m-4 form-new active" id="view" role="tabpanel" aria-labelledby="view-tab">
                         <h3 class="register-heading"> View Form </h3>
                         <div class="row register-form">
                             <div class="col-md-12">
                                 <form method="post">
                                     <div class="form-group">
-                                        <input type="text" name="LGform1_user" class="form-control" placeholder="Your Email *" value="" required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="LGform1_pwd" class="form-control" placeholder="Your Password *" value="" required="" />
+                                        <input type="text" name="LGform1_user" class="form-control" placeholder="Input ID" value="" required="" />
                                     </div>
                                     <div class="form-group text-center">
-                                        <input type="submit" name="LGform1" class="btnContactSubmit" value="Login" />
-
+                                        <input type="submit" name="LGform1" class="btnContactSubmit" value="Search " />
+                                        <input type="submit" name="LGform2" class="btnContactSubmit" value="Search Specific ID " />
                                     </div>
                                 </form>
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade show active text-align form-new" id="insert" role="tabpanel" aria-labelledby="insert-tab">
+                    <div class="tab-pane fade show  text-align form-new" id="insert" role="tabpanel" aria-labelledby="insert-tab">
                         <h3 class="register-heading text-center m-4">Insert Form</h3>
                         <div class="row register-form">
                             <div class="col-md-12">
