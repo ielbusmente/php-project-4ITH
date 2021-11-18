212f747b8bd8f2e6e0c3fcfcd1d19da52d21e0a4
ielbusmente <ieljbusmente@gmail.com>
2021-11-18 17:37:25 +0800

    db connection

--- Exercise6/index.php
+++ Exercise6/index.php
@@ -1,3 +1,61 @@
+<?php
+function dbconnect($action, $input)
+{
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'sleepyDB';
+
+    $conn = mysqli_connect($dbhost, $dbuser,  $dbpass, $dbname);
+    if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
+    }
+    if ($action === 'insert') {
+        $conn->query("INSERT INTO `adminUser` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES ('" . $input[0] . "', '" . $input[1] . "', '" . $input[2] . "', '" . $input[3] . "', '" . $input[4] . "')");
+    } else {
+        if ($action === 'view_one') {
+            $sql = "SELECT id, firstName, lastName, email FROM adminUser WHERE id = " . $input;
+        }
+        if ($action === 'view_all') {
+            $sql = "SELECT id, firstName, lastName, email FROM adminUser";
+        }
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "User ID: " . $row['id'] . "<br/>First Name: " . $row['firstName'] . "<br/>Last Name: " . $row['lastName'] . "<br/>Email: " . $row['email'] . "<br/><br/>";
+            }
+        } else {
+            echo '0 results';
+        }
+    }
+    $conn->close();
+    // $dbhost = 'localhost';
+    // $dbuser = 'root';
+    // $dbpass = '';
+    // $dbname = 'sleepyDB';
+
+    // $conn = mysqli_connect($dbhost, $dbuser,  $dbpass, $dbname);
+    // if ($conn->connect_error) {
+    //     die("Connection Failed: " . $conn->connect_error);
+    // }
+
+    // $conn->query("INSERT INTO `adminuser` (`id`, `firstName`, `lastName`, `email`, `password`) VALUES ('1', 'Jose', 'Rizal', 'jrizal@gmail.com', '123123aA'), ('2', 'Andres', 'Bonifacio', 'abonifacio@gmail.com', '123123aA')");
+    // $sql = "SELECT id, firstName, lastName, email FROM adminUser";
+    // $result = $conn->query($sql);
+
+    // if ($result->num_rows > 0) {
+    //     while ($row = $result->fetch_assoc()) {
+    //         echo "User ID: " . $row['id'] . "First Name: " . $row['firstName'] . "Last Name: " . $row['lastName'] . "Email: " . $row['email'] . "<br/>";
+    //     }
+    // } else {
+    //     echo '0 results';
+    // }
+
+    // $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -67,8 +125,15 @@
                                         <input type="text" name="id" class="form-control" placeholder="ID" value="" required="" />
                                     </div>
                                     <div class="form-group text-center">
-                                        <input type="submit" name="submit" class="btnContactSubmit" value="Submit" />
+                                        <input type="submit" name="insert" class="btnContactSubmit" value="Insert" />
                                     </div>
+                                    <?php
+                                    if (isset($_POST['insert'])) {
+                                        if (!$_POST['id'] && !$_POST['firstName'] && !$_POST['lastName'] && !$_POST['email'] && !$_POST['password']) {
+                                            dbconnect('insert', [$_POST['id'], $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['password']]);
+                                        }
+                                    }
+                                    ?>
                                 </form>
                             </div>
                         </div>
