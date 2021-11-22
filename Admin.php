e4d40199feea2ca4eb7e318295e9985ac6dc7b76
nmsarm <daneah.sarmiento@yahoo.com>
2021-11-22 15:21:43 +0800

    added page templates

--- Admin.php
+++ Admin.php
@@ -16,7 +16,7 @@ if (isset($_POST['logout'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Admin.css">
+    <link rel="stylesheet" href="css/admin.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="https://kit.fontawesome.com/274d8634a7.js" crossorigin="anonymous"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
