f9dcf45ae8f3120871e7cb93d17e660629f64ff1
nmsarm <daneah.sarmiento@yahoo.com>
2021-11-22 18:15:31 +0800

    footer mod

--- contact.php
+++ contact.php
@@ -79,8 +79,6 @@
             <br><br><br>
         </section>
         <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
-        </footer>
+        <?php include 'footer.php';?>
     </body>
 </html>
--- products.php
+++ products.php
@@ -237,8 +237,6 @@
             </div>
         </section>
         <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
-        </footer>
+        <?php include 'footer.php';?>
     </body>
 </html>
