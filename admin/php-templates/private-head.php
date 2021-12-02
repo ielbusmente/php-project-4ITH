7c8288cc1afee3d91f14fb1570d61575e4621d23
ielbusmente <ieljbusmente@gmail.com>
2021-12-02 18:44:52 +0800

    moved style tag

--- admin/php-templates/private-head.php
+++ admin/php-templates/private-head.php
@@ -1,9 +1,7 @@
 <!-- Favicon -->
 <link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
 <!-- <link rel="stylesheet" href="../css/admin.css"> -->
-<style>
-    <?php include "../css/admin-css.php" ?>
-</style>
+<?php include "../css/admin-css.php" ?>
 <script src="https://kit.fontawesome.com/274d8634a7.js" crossorigin="anonymous"></script>
 <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet"> -->
 <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
