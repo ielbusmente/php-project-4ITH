a3cda103e8705222ef87b137d51ae058f260c406
ielbusmente <ieljbusmente@gmail.com>
2021-12-05 01:09:30 +0800

    local code

--- php-templates/dbconnect.php
+++ php-templates/dbconnect.php
@@ -1,14 +1,14 @@
 <?php
 //local code
 $dbhost = 'localhost';
-// $dbuser = 'root';
-// $dbpass = '';
-// $dbname = 'sleepyDB';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'sleepyDB';
 //prod code
 // $dbhost = '';
-$dbuser = 'id18063116_admin';
-$dbpass = '<6rePiXHfC}L*(y#';
-$dbname = 'id18063116_sleepyph';
+// $dbuser = 'id18063116_admin';
+// $dbpass = '<6rePiXHfC}L*(y#';
+// $dbname = 'id18063116_sleepyph';
 // id18063116_sleepyph
 $conn = mysqli_connect($dbhost, $dbuser,  $dbpass, $dbname);
 if ($conn->connect_error) {
