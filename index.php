9b4f578f016a433feb318eac04b7e1551fa4e7ce
nmsarm <daneah.sarmiento@yahoo.com>
2021-12-08 00:30:28 +0800

    index.php fix

--- index.php
+++ index.php
@@ -24,40 +24,40 @@
             <div class="carousel-item active">
             <div class="mask flex-center">
                 <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-md-7 col-12 order-md-1 order-2" style="top: 190px;">
-                        <h4>High-Quality <br>
-                            Sleeping Essentials <br>Just For You</h4>  
-                        <p>Providing you with comfy and classy sleepwears without breaking your pocket!</p>
-                        <p>DTI Registered.</p>
-                        <a href="products.php">View Products</a> 
+                    <div class="row align-items-center">
+                        <div class="col-md-7 col-12 order-md-1 order-2" style="top: 190px;">
+                            <h4>High-Quality <br>
+                                Sleeping Essentials <br>Just For You</h4>  
+                            <p>Providing you with comfy and classy sleepwears without breaking your pocket!</p>
+                            <p>DTI Registered.</p>
+                            <a href="products.php">View Products</a> 
+                        </div>
+                        <div class="mySlides fadein col-md-5 col-12 order-md-2 order-1 ">
+                            <?php 
+                            // display images from directory
+                            $dir = "assets/img/banner/";
+                            
+                            $scan_dir = scandir($dir); //list files inside the directory
+                            foreach($scan_dir as $img):
+                                if(in_array($img,array('.','..'))){
+                                    continue;
+                                }   
+                            ?>
+                            <img src="<?php echo $dir.$img ?>" class="mx-auto" style="height: 640px;" alt="<?php echo $img ?>">
+                            <?php endforeach; ?>
+                        </div>
                     </div>
-                    <div class="mySlides fadein col-md-5 col-12 order-md-2 order-1 ">
-                        <?php 
-                        // display images from directory
-                        $dir = "assets/img/banner/";
-                        
-                        $scan_dir = scandir($dir); //list files inside the directory
-                        foreach($scan_dir as $img):
-                            if(in_array($img,array('.','..'))){
-                                continue;
-                            }   
-                        ?>
-                        <img src="<?php echo $dir.$img ?>" class="mx-auto" style="height: 640px;" alt="<?php echo $img ?>">
-                        <?php endforeach; ?>
-                    </div>
-                </div>
+                </div>         
             </div>
         </div>
-        <!--slide end-->
-
-        <section class="product-category section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="title text-center">
-                            <h2> Product Categories </h2>
-                        </div>
+    </div> 
+    <!--slide end-->
+    <section class="product-category section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="title text-center">
+                        <h2> Product Categories </h2>
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -67,7 +67,7 @@
                             <div class="content">
                                 <h3>Sleepwear</h3>
                             </div>
-                        </a>	
+                        </a>
                     </div>
                 </div>
                 <div class="col-md-6">
@@ -77,46 +77,46 @@
                             <div class="content">
                                 <h3>Essentials</h3>
                             </div>
-                        </a>	
+                        </a>
                     </div>
                 </div>
             </div>
-        </section>
-
+        </div>
+    </section>
 
     <!-- Icons Grid-->
     <section class="features-icons bg-light text-center">
-        <div class="container">
-            <div class="row">
-                <?php
-                    $img = ['assets/icons/handwash.png','assets/icons/detergent.png','assets/icons/bleach.png'];
-                    $h3 = [ 'hand wash only','use mild detergent','no bleach'];
-                    for ($i = 0; $i < 3; $i++) {
-                    ?>
-                        <div class="col-lg-4">
-                            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                                <div class="features-icons-icon d-flex mb-4"><img src="<?php echo $img[$i] ?>" width="100" class="m-auto text-primary" alt=""></div>
-                                <h3 style="color: #bfad6d"><?php echo $h3[$i] ?></h3>
-                            </div>
+    <div class="container">
+        <div class="row">
+            <?php
+                $img = ['assets/icons/handwash.png','assets/icons/detergent.png','assets/icons/bleach.png'];
+                $h3 = [ 'hand wash only','use mild detergent','no bleach'];
+                for ($i = 0; $i < 3; $i++) {
+                ?>
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <div class="hover-spin features-icons-icon d-flex mb-4"><img src="<?php echo $img[$i] ?>" width="100" class="m-auto text-primary" alt=""></div>
+                            <h3 style="color: #bfad6d"><?php echo $h3[$i] ?></h3>
                         </div>
-                    <?php } ?>
-            </div>
-        </section>
+                    </div>
+                <?php } ?>
+        </div>
+    </section>
 
-        <!-- Footer-->
-        <?php include 'php-templates/footer.php'; ?>
+    <!-- Footer-->
+    <?php include 'php-templates/footer.php'; ?>
 
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-        <script>
-            $(function() {
-                $('.fadein img:gt(0)').hide();
-                setInterval(function() {
-                    $('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');
-                }, 5000);
-            });
-        </script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script>
+        $(function() {
+            $('.fadein img:gt(0)').hide();
+            setInterval(function() {
+                $('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');
+            }, 5000);
+        });
+    </script>
 
 </body>
 
